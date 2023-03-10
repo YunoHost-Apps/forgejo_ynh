9ad40a3ac229bd672b1d12dcbb237a54d0772a4e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-10 17:22:52 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,3 @@
 #=================================================
 # SET ALL CONSTANTS
 #=================================================
-
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql"
